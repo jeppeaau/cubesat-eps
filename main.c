040998636bcc61720251decbd3785eb0e9e931c4
Kasper Steensig <kasper.steensig@gmail.com>
2018-05-14 13:25:40 +0200

    clean up

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <avr/eeprom.h>
 #include <util/delay.h>
+#include "adc.h"
 #include "i2cmaster.h"
 #include "krnl.h"
 #include "onlaunch.h"
@@ -17,17 +18,17 @@ void update_bootcount() { bootcount++; }
 uint32_t read_bootcount() { return bootcount; }
 
 int main() {
-  /*
-bootcount = read_bootcount();
-
-if (bootcount == 0) {
-  launch_sequence();
-}
-
-update_bootcount();
-*/
-
   init_pwm(175, DDD6);
+
+  bootcount = read_bootcount();
+
+  if (bootcount == 0) {
+    launch_sequence();
+  }
+
+  update_bootcount();
+
+  i2c_init();
 
   boost_converter_ina = init_ina219(0x40, 6559, 27307);
   configurate_ina219(&boost_converter_ina);
@@ -35,10 +36,8 @@ update_bootcount();
 
   mppt = init_mppt(5, 175);
 
-  i2c_init();
-
-  k_crt_task(boost_converter_sensor_task, 1, boost_converter_ina_stack, 50);
-  k_crt_task(mppt_task, 1, mppt_task, 100);
+  k_crt_task(boost_converter_sensor_task, 1, boost_converter_ina_stack, 25);
+  k_crt_task(mppt_task, 1, mppt_stack, 50);
 
   // init krnl so you can create 2 tasks, no semaphores and no message
   // queues
